2ca2c081878dd2f405f6af6c19545ea97992e403
leolang123 <143671478+leolang123@users.noreply.github.com>
2024-10-31 14:04:42 +1100

    Set up start menu

---
+++ Assets/Script/StartMenuController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void LoadLevel2()
+    {
+        
+    }
+}
